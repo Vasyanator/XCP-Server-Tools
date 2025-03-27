10fbd62d977ac932e4c63ca60b17e7bee577668e
Vasyanator <135327897+Vasyanator@users.noreply.github.com>
2025-03-27 11:34:20 +0400

    Update main.py

--- tab_rogue_buffs/main.py
+++ tab_rogue_buffs/main.py
@@ -5,6 +5,7 @@ from tkinter import VERTICAL, RIGHT, LEFT, Y, StringVar, messagebox
 
 from tab_rogue_buffs.virtual_universe_blessings import VirtualUniverseBlessingsTab
 from tab_rogue_buffs.virtual_universe_miracles import VirtualUniverseMiraclesTab
+from tab_rogue_buffs.virtual_universe_extras import VirtualUniverseMiscTab
 
 class RogueBuffsTab:
     def __init__(self, notebook, rogue_buffs_su, rogue_buffs_food, rogue_buffs_various, rogue_buffs_from_entities, rogue_buffs_other, rogue_buffs_unknown, rogue_miracles, command_manager, localization):
@@ -40,6 +41,15 @@ class RogueBuffsTab:
         self.virtual_universe_miracles_tab = VirtualUniverseMiraclesTab(
             self.sub_notebook,
             self.rogue_miracles,
+            self.command_manager,
+            self.localization
+        )
+        self.virtual_universe_misc_tab = VirtualUniverseMiscTab(
+            self.sub_notebook,
+            self.rogue_buffs_food,
+            self.rogue_buffs_various,
+            self.rogue_buffs_from_entities,
+            self.rogue_buffs_other,
             self.command_manager,
             self.localization
         )
